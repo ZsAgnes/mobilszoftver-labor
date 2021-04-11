2adde87993f1583d2ba98b2e3712abe97e2a7ccd
Zsako Agnes <zs.agi27@gmail.com>
2021-04-11 21:42:19 +0200

    Add details screen

---
+++ app/src/main/java/com/example/news/ui/details/DetailsViewModel.kt
@@ -0,0 +1,17 @@
+package com.example.news.ui.details
+
+import co.zsmb.rainbowcake.base.RainbowCakeViewModel
+import javax.inject.Inject
+
+class DetailsViewModel @Inject constructor(
+    private val detailsPresenter: DetailsPresenter
+) : RainbowCakeViewModel<DetailsViewState>(
+    Loading
+) {
+
+    fun load() = execute {
+        viewState =
+            DetailsReady(detailsPresenter.getData())
+    }
+
+}
